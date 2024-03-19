1925485e7872bf9b925e8b566f4dc6f2dbfa7c41
sipheng2023 <123350069+sipheng2023@users.noreply.github.com>
2024-03-19 18:34:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import TodoList from './components/TodoList'
+import Input from './components/Input'
+import Button from './components/Button'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+
+  width: 100vw;
+  height: 100vh;
+  background-color: #f6f7f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+`
+
+function App() {
+
+  const [todoList, setTodoList] = useState([])
+  const [text, setText] = useState("")
+
+  const onTextChange = ({target: {value}}) => {
+
+    setText(value)
+
+  }
+
+  const addTask = () => {
+
+    // add todo
+    setTodoList([
+      ...todoList,
+      text
+    ])
+
+    // clear text
+    setText("")
+
+  }
+
+  const removeTodo = (index) => {
+
+    let todo = [...todoList]
+
+    todo.splice(index, 1)
+    setTodoList(todo)
+
+  }
+
+  return (
+   
+    <Container>
+ <h1> Welcome to Sipheng Todo List</h1>
+      <div>
+        <Input type="text" value={text} onChange={ onTextChange } />
+        <Button onClick={addTask} >Add task</Button>
+      </div>
+
+      <TodoList todoList={todoList} removeTodo={removeTodo} />
+
+        
+      </Container>
+  )
+
+}
+
+export default App
+
